83b375260c4d583ebc919fc8a27a7f8bab039c35
jai2201 <jai_s@me.iitr.ac.in>
2022-12-31 04:23:34 +0530

    file-structure: Setup basic file-structure for the project.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { React, useEffect, useState } from 'react';
+
+import ContactList from './containers/ContactList';
+import SearchBox from './components/SearchBox';
+
 function App() {
-  return <div className="App"></div>;
+  const [contacts, setContacts] = useState([]);
+  return (
+    <div className="App">
+      <div className={'bg-gray-100 min-h-screen p-20'}>
+        <div>
+          <p className="text-4xl font-semibold">Contacts</p>
+        </div>
+        <section>
+          <SearchBox />
+        </section>
+        <section className={'grid sm:grid-cols-2 md:grid-cols-4 gap-6 p-10'}>
+          <ContactList contacts={contacts} />
+        </section>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/containers/ContactList.js
@@ -0,0 +1,5 @@
+const ContactList = ({ contacts }) => {
+  return <div></div>;
+};
+
+export default ContactList;
