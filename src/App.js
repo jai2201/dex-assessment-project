fd72bed40f06792094b17908fb7e4aa6305fc9b6
jai2201 <jai_s@me.iitr.ac.in>
2022-12-31 15:13:32 +0530

    create-contact: Add functionality to upload contacts.
    Upload image to S3 bucket at first and then store the url in
    dynamoDB with the rest of the body of contact object.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import { React, useEffect, useState } from 'react';
 import axios from 'axios';
+import S3FileUpload from 'react-s3';
 import ContactList from './containers/ContactList';
 import SearchBox from './components/SearchBox';
 import AddModal from './common/AddModal';
+window.Buffer = window.Buffer || require('buffer').Buffer;
+
+const S3_BUCKET = 'dex-assessment-contact-images';
+const REGION = 'ap-south-1';
+const ACCESS_KEY = 'AKIAUTLN6U54YCE4MJYH';
+const SECRET_ACCESS_KEY = '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';
 
 function App() {
   const [contacts, setContacts] = useState([]);
   const [addModalShow, setAddModalShow] = useState(false);
-
+  const config = {
+    bucketName: S3_BUCKET,
+    region: REGION,
+    accessKeyId: ACCESS_KEY,
+    secretAccessKey: SECRET_ACCESS_KEY,
+  };
   const [values, setValues] = useState({
     name: '',
     image: null,
@@ -23,8 +35,42 @@ function App() {
     }
   };
 
-  const handleSaveContact = () => {
-    console.log(values);
+  const handleSaveContact = async () => {
+    try {
+      console.log('request received');
+      const imageS3URL = await handleUpload(values.image);
+      console.log('uploading data to db now', imageS3URL);
+      const response = axios
+        .post(
+          `https://14jpf5t9kc.execute-api.ap-south-1.amazonaws.com/prod/contacts`,
+          {
+            id: (Math.random() + 1).toString(36).substring(7),
+            name: values.name,
+            image: imageS3URL,
+            phone: values.phone,
+            last_contacted_at: values.lastContactDate,
+          }
+        )
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+      getContacts();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleUpload = async (file) => {
+    console.log('request received for uploading file');
+    try {
+      const uploadedImage = await S3FileUpload.uploadFile(file, config);
+      return uploadedImage.location;
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   const getContacts = async () => {
